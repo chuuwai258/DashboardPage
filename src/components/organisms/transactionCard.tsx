2d6e59af397aa3baf1d203432989675d9165734e
chuuwai258 <chuuwaihnin@gmail.com>
2026-07-14 23:03:48 +0630

    feat:dashboard

--- src/components/organisms/transactionCard.tsx
+++ src/components/organisms/transactionCard.tsx
@@ -11,16 +11,15 @@ import ChartBarDefault from '../molecules/transaction-barchart'
 const TransactionCard = () => {
   return (
     <>
-      <div className="flex flex-col md:flex-row  lg:flex-row gap-4 overflow-x-auto md:overflow-visible px-7 py-4  lg:gap-9 md:gap-7 lg:px-15 ">
-        
-
+      <div className="flex flex-col md:flex-row lg:flex-row gap-4 overflow-x-hidden px-7 py-4 lg:gap-8 lg:mx-5  md:gap-4">
+        it
         {/* First Card */}
-        <div className="flex gap-10 md:gap-5 lg:gap-9 md:w-[75%]">
+        <div className="flex gap-10 md:gap-4 lg:gap-6">
           <div className="min-w-[300px] sm:min-w-[340px] md:min-w-0">
             <h2 className="text-xl mb-5 text-primary2 font-semibold">
               My Cards
             </h2>
-            <Card className="bg-[#0A06F4] text-white md:h-64 lg:h-65">
+            <Card className="bg-[#0A06F4] text-white md:h-64 lg:h-65 lg:w-full">
               <CardHeader className="flex justify-between md:h-12">
                 <div>
                   <h1 className="md:text-base lg:text-lg text-white/70">
@@ -37,8 +36,8 @@ const TransactionCard = () => {
                 </div>
               </CardHeader>
 
-              <CardContent className='h-22'>
-                <div className="flex md:gap-12 lg:gap-15">
+              <CardContent className="h-22">
+                <div className="flex md:gap-8 lg:gap-10">
                   <div>
                     <h1 className="uppercase text-white/70 md:text-base lg:text-lg">
                       Card Holder
@@ -54,10 +53,10 @@ const TransactionCard = () => {
                 </div>
               </CardContent>
 
-              <CardFooter className=" flex md:h-10 lg:h-16 items-center justify-between border-t md:pb-8">
-                <h1 className="md:text-base lg:text-2xl ">3778 **** **** 1234</h1>
+              <CardFooter className=" flex md:h-13 lg:h-16 items-center justify-between border-t md:pb-8">
+                <h1 className="md:text-lg lg:text-2xl">3778 **** **** 1234</h1>
                 <img
-                  src={Image.cardImg2}
+                  src={Image.cardImg4}
                   alt="cardImg2"
                   className="md:size-8 lg:size-10"
                 />
@@ -66,10 +65,10 @@ const TransactionCard = () => {
           </div>
           {/* Second Card */}
           <div className="min-w-[300px] sm:min-w-[340px] md:min-w-0">
-            <h2 className="text-lg mb-5 ml-50 md:ml-30 lg:ml-47 text-primary2 font-semibold">
+            <h2 className="text-lg mb-5 ml-50 md:ml-40 lg:ml-55 text-primary2 font-semibold">
               +Add Card
             </h2>
-            <Card className="md:h-64 lg:h-65">
+            <Card className="  md:h-64 lg:h-65">
               <CardHeader className="flex justify-between">
                 <div>
                   <CardDescription className="md:text-base lg:text-lg">
@@ -86,7 +85,7 @@ const TransactionCard = () => {
                 </div>
               </CardHeader>
 
-              <CardContent  className='h-22'>
+              <CardContent className="h-22">
                 <div className="flex md:gap-10 lg:gap-15">
                   <div>
                     <CardDescription className="uppercase md:text-base lg:text-lg">
@@ -103,8 +102,8 @@ const TransactionCard = () => {
                 </div>
               </CardContent>
 
-              <CardFooter className=" flex md:h-13 lg:h-16 items-center justify-between border-t md:pb-8">
-                <h1 className="md:text-sm lg:text-2xl">3778 **** **** 1234</h1>
+              <CardFooter className=" flex md:h-13 lg:h-20 items-center justify-between border-t md:pb-8 lg:pb-15">
+                <h1 className="md:text-lg lg:text-2xl ">3778 **** **** 1234</h1>
                 <img
                   src={Image.cardImg4}
                   alt="cardImg2"
@@ -116,14 +115,13 @@ const TransactionCard = () => {
         </div>
         <div>
           {/* Card Transaction */}
-        <div className="min-w-[300px] sm:min-w-[340px] md:min-w-0 lg:min-w-[40px]">
-          <h2 className="text-[18px] mb-5 text-primary2 font-semibold">
-            My Expense
-          </h2>
-          <ChartBarDefault />
+          <div className="min-w-[300px] sm:min-w-[340px] md:min-w-0 lg:min-w-[40px]">
+            <h2 className="text-[18px] mb-5 text-primary2 font-semibold">
+              My Expense
+            </h2>
+            <ChartBarDefault />
+          </div>
         </div>
-          </div>       
-        
       </div>
     </>
   )
